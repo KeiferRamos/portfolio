9c39ae1b66d73840f4a4a7c460567f8e35a95bbe
KeiferRamos <keiferramos2001@gmail.com>
2022-05-27 09:48:02 +0800

    gg

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import Navbar from "./components/navbar";
 import Routes from "./router/router";
 import "./styles/app.css";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/");
+  }, []);
   return (
     <div className="App">
       <Navbar />
